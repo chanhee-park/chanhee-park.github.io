e6fc1793e3ce4c40778f9932571f2d48f1827f35
chanhee13p <chanhee13p@gmail.com>
2020-04-17 18:10:31 -0700

    add some dist file

---
+++ dist/data.js
@@ -0,0 +1,104 @@
+var Data = {
+  about: ["\uC81C \uBAA9\uD45C\uB294 \uBCF5\uC7A1\uD55C \uB370\uC774\uD130\uB97C \uC774\uD574\uD558\uB294 \uC7A5\uBCBD\uC744 \uB0AE\uCD94\uB294 \uAC83\uC785\uB2C8\uB2E4. \n  \uD2B9\uD788 \uC81C \uC5F0\uAD6C \uBE44\uC804\uC740 '\uC2DC\uAC01\uD654\uB97C \uD1B5\uD55C \uBA38\uC2E0\uB7EC\uB2DD \uBAA8\uB378 \uD574\uC11D\uACFC \uAC1C\uC120'\uC5D0 \uC788\uC2B5\uB2C8\uB2E4. \n  \uBA38\uC2E0\uB7EC\uB2DD \uBAA8\uB378\uC740 \uB192\uC740 \uC131\uB2A5\uC5D0\uB3C4 \uBD88\uAD6C\uD558\uACE0 \uD574\uC11D\uACFC \uB514\uBC84\uAE45\uC774 \uC5B4\uB835\uB2E4\uB294 \uC774\uC720\uB85C \uD65C\uC6A9\uC5D0 \uC5B4\uB835\uACE4 \uD569\uB2C8\uB2E4. \n  \uBA38\uC2E0\uB7EC\uB2DD \uBAA8\uB378\uC758 \uD589\uB3D9 \uD328\uD134\uC744 \uC774\uD574\uD558\uB294 \uAC83\uC740 \uC778\uACF5\uC9C0\uB2A5 \uBAA8\uB378\uC744 \uAC1C\uC120\uD558\uACE0 \uD65C\uC6A9\uD558\uB294\uB370 \uC704\uD574 \uD544\uC218\uC801\uC785\uB2C8\uB2E4. \n  \uBA38\uC2E0\uB7EC\uB2DD \uBAA8\uB378\uC758 \uC774\uD574\uD558\uAE30 \uC5B4\uB824\uC6B4 \uC774\uC720\uB294 \uD559\uC2B5\uC5D0 data, parameter, hyperparams\uB97C \uBE44\uB86F\uD55C \uB9CE\uC740 \uBCC0\uC218\uAC00 \uC0AC\uC6A9\uB418\uAE30 \uB54C\uBB38\uC785\uB2C8\uB2E4. \n  \uB370\uC774\uD130 \uC2DC\uAC01\uD654\uB294 \uB9CE\uC740 \uB370\uC774\uD130 \uC18D\uC5D0\uC11C \uD328\uD134\uC744 \uCC3E\uB294\uB2E4\uB294 \uAC15\uC810\uC774 \uC788\uC2B5\uB2C8\uB2E4. \n  \uC778\uACF5\uC9C0\uB2A5 \uC124\uBA85\uC5D0 \uB370\uC774\uD130 \uC2DC\uAC01\uD654\uAC00 \uD575\uC2EC \uC5ED\uD560\uC744 \uD560 \uAC83\uC774\uB77C\uACE0 \uC0DD\uAC01\uD569\uB2C8\uB2E4.", "\uC800\uB294 \uBA38\uC2E0\uB7EC\uB2DD \uBAA8\uB378\uC774 \uB354 \uB192\uC740 \uC131\uB2A5\uACFC \uD574\uC11D \uAC00\uB2A5\uC131\uC744 \uAC16\uB3C4\uB85D \uB9CC\uB4E4\uAE30 \uC704\uD55C \uB370\uC774\uD130 \uC2DC\uAC01\uD654 \uC2DC\uC2A4\uD15C\uC744 \uB9CC\uB4E4\uC5B4 \uC654\uC2B5\uB2C8\uB2E4. \n  \uCC38\uC2E0\uD558\uACE0 \uD6A8\uACFC\uC801\uC778 \uB370\uC774\uD130 \uBD84\uC11D \uC2DC\uC2A4\uD15C\uC744 \uB9CC\uB4E4\uAE30 \uC704\uD574 \uC6F9 \uD504\uB860\uD2B8\uC5D4\uB4DC, \uB370\uC774\uD130 \uC2DC\uAC01\uD654, \uBA38\uC2E0\uB7EC\uB2DD\uC744 \uACF5\uBD80\uD558\uACE0 \uC788\uC2B5\uB2C8\uB2E4. "],
+  experiences: [{
+    "where": "UC Davis VIDI Labs",
+    "status": "Visiting Researcher",
+    "href": "https://vidi.cs.ucdavis.edu/",
+    "period": "2020.03 - 현재",
+    "story": "\n      VIDI Labs \uB294 \uCE98\uB9AC\uD3EC\uB2C8\uC544 \uC8FC\uB9BD\uB300\uD559 \uB370\uC774\uBE44\uC2A4 \uCEA0\uD37C\uC2A4 \uCEF4\uD4E8\uD130\uACF5\uD559\uACFC\uC758 \uB370\uC774\uD130 \uC2DC\uAC01\uD654 \uC5F0\uAD6C\uC2E4\uC785\uB2C8\uB2E4.\n      \uB370\uC774\uD130 \uC2DC\uAC01\uD654 \uBD84\uC57C\uC758 \uAD8C\uC704\uC788\uB294 \uC5F0\uAD6C\uC790\uC774\uC2E0 Kwan-Liu Ma \uAD50\uC218\uB2D8\uC758 \uC9C0\uB3C4 \uC544\uB798\uC11C \n      \uBA38\uC2E0\uB7EC\uB2DD \uBAA8\uB378\uC758 \uC131\uB2A5 \uD3C9\uAC00 \uC5F0\uAD6C\uC640 \uB300\uC6A9\uB7C9 \uB124\uD2B8\uC6CC\uD06C \uBE44\uAD50\uB97C \uC704\uD55C \uC2DC\uAC01\uD654 \uC5F0\uAD6C\uB97C \uC218\uD589\uD558\uACE0 \uC788\uC2B5\uB2C8\uB2E4.\n      \uC774\uACF3\uC5D0\uC11C \uC5F0\uAD6C\uD558\uB294 \uB3D9\uC548 \uC5F0\uAD6C\uC758 \uAC00\uCE58\uB97C \uD310\uB2E8\uD558\uB294 \uC548\uBAA9\uACFC \uC601\uC5B4 \uC2E4\uB825\uC744 \uD5A5\uC0C1\uC2DC\uCF1C \uAE00\uB85C\uBC8C \uC778\uC7AC\uAC00 \uB418\uACE0\uC790 \uD569\uB2C8\uB2E4.\n      "
+  }, {
+    "where": "(주) 더더랩",
+    "status": "Software Membership & Intern",
+    "href": "https://www.thethelab.io/",
+    "period": "2017.04 - 2019.02",
+    "story": "\n      \uB354\uB354\uB7A9\uC740 \uC18C\uD504\uD2B8\uC6E8\uC5B4\uB97C \uAC1C\uBC1C\uD558\uACE0 \uACF5\uAE09\uD558\uB294 \uD68C\uC0AC\uC785\uB2C8\uB2E4.\n      \uC800\uB294 \uC774 \uD68C\uC0AC\uC5D0\uC11C \uC6F9 \uD504\uB860\uD2B8\uC5D4\uB4DC \uAC1C\uBC1C\uC790\uB85C\uC11C \uC5ED\uB7C9\uC744 \uAC08\uACE0 \uB2E6\uC558\uC2B5\uB2C8\uB2E4.\n      2017\uB144 \uBD04, \uD559\uAD50\uC758 \uC18C\uAC1C\uB85C \uBA64\uBC84\uC2ED \uD504\uB85C\uADF8\uB7A8\uC744 \uC2DC\uC791\uD588\uC2B5\uB2C8\uB2E4. \uBA64\uBC84\uC2ED \uD504\uB85C\uADF8\uB7A8\uC774 \uC885\uB8CC\uB41C \uC774\uD6C4\uC5D0\uB3C4 \uC778\uD134\uC73C\uB85C \uADFC\uBB34\uD588\uC2B5\uB2C8\uB2E4.\n      \uB354\uB354\uB7A9\uC5D0\uC11C \uADFC\uBB34\uD558\uB294 \uB3D9\uC548 TypeScript, Vue, Firebase \uB4F1 \uB2E4\uC591\uD55C \uC6F9 \uAC1C\uBC1C \uAE30\uC220 \uC2A4\uD0DD\uC744 \uC313\uC558\uC2B5\uB2C8\uB2E4.\n      \uCC38\uC5EC\uD55C \uD504\uB85C\uC81D\uD2B8\uC5D0\uC11C\uB294 \uC8FC\uB85C \uC6F9 \uD504\uB860\uD2B8\uC5D4\uB4DC\uC640 \uB370\uC774\uD130 \uC2DC\uAC01\uD654 \uAC1C\uBC1C\uC744 \uB9E1\uC558\uC2B5\uB2C8\uB2E4.\n      "
+  }],
+  researches: [{
+    id: 'visml',
+    title: 'A Visualization System for Performance Analysis of Image Classification Models',
+    authors: ['Chanhee Park', 'Hyojin Kim', 'Kyungwon Lee'],
+    conf_title: 'IS&T International Symposium on Electronic Imaging',
+    conf_short: "EI'20",
+    short_conf: 'ei',
+    year: '2020',
+    thumb: 'research/2020-ei-visml-thumb.png',
+    code: 'https://github.com/chanhee-park/VisMlic',
+    site: 'https://chanhee-park.github.io/VisMlic/',
+    video: true,
+    pdf: true
+  }, {
+    id: 'color',
+    title: 'A Study of Colormaps in Network Visualization',
+    authors: ['Raja Mubashar Karim', 'Oh-Hyun Kwon', 'Chanhee Park', 'Kyungwon Lee'],
+    conf_title: 'MDPI Applied Sciences',
+    short_conf: 'mdpi',
+    conf_short: 'Appl. Sci. 2019, 9(20)',
+    year: '2019',
+    pdf: true
+  }, {
+    id: 'summ',
+    title: 'A Narrative Topic Map Visualization to Summarize and Recall a Meeting',
+    authors: ['Suhyun Lim', 'Chanhee Park', 'Hyunwoo Han', 'Jaejong Ho', 'Junyup Hong', 'Soojung Lee', 'Kyunwon Lee'],
+    conf_title: 'IEEE Visualization Conference',
+    conf_short: "InfoVis'19",
+    short_conf: 'vis',
+    year: '2019',
+    thumb: true,
+    pdf: true,
+    poster: true,
+    video: true // ToDO: Add it
+  }, {
+    id: 'gitvis',
+    title: 'GitViz: An Interactive Visualization System for Analyzing Development Trends in the Open- Source Software Community',
+    authors: ['Chanhee Park', 'Sungjun Do', 'Eunjeong Lee', 'Hanna Jang', 'Sungchan Jeong', 'Hyunwoo Han', 'Kyungwon Lee'],
+    conf_title: 'IEEE Pacific Visualization Symposium',
+    conf_short: "PacificVis'19",
+    short_conf: 'pvis',
+    year: '2019',
+    thumb: true,
+    pdf: true,
+    video: true // ToDo: Update it
+  }, {
+    id: 'visml',
+    title: 'ComDia+: An Interactive Visual Analytics System for Comparing, Diagnosing, and Improving Multiclass Classifiers',
+    authors: ['Chanhee Park', 'Jina Lee', 'Hyunwoo Han', 'Kyungwon Lee'],
+    conf_title: 'IEEE Pacific Visualization Symposium',
+    short_conf: 'pvis',
+    conf_short: "PacificVis'19",
+    year: '2019',
+    pdf: true
+  }, {
+    id: 'congress',
+    title: 'A visual analysis of power relations among people on time-series data using network visualization',
+    authors: ['Gyeongcheol Choi', 'Taerin Yoon', 'Chanhee Park', 'Sarah Lee', 'Hogwon Choi', 'Jieun Kang', 'Chulwoong Park', 'Seongmin Mun', 'Kyungwon Lee'],
+    conf_title: 'ocial Science History Association',
+    conf_short: "SSHA'18",
+    short_conf: 'ssha',
+    year: '2018',
+    thumb: true,
+    pdf: true
+  }, {
+    id: 'gitvis',
+    title: 'An Interactive Visualization To Help You Gain Knowledge from Authoritative Developers',
+    authors: ['Chanhee Park', 'Sungjun Do', 'Eunjeong Lee', 'Hanna Jang', 'Sungchan Jeong', 'Hyunwoo Han', 'Kyungwon Lee'],
+    conf_title: 'IEEE Visualization Conference',
+    conf_short: "InfoVis'18",
+    short_conf: 'vis',
+    year: '2018',
+    pdf: true,
+    poster: true
+  }],
+  educations: [{
+    "course": "석사 과정",
+    "where": "아주대학교 라이프미디어 협동과정",
+    "href": "http://www.ajou.ac.kr/dt/index.jsp",
+    "period": "2019-재학 중",
+    "story": "\uB370\uC774\uD130 \uC2DC\uAC01\uD654\uC5D0 \uB300\uD55C \uB354 \uC2EC\uB3C4 \uAE4A\uC740 \uC5F0\uAD6C\uB97C \uD558\uAE30 \uC704\uD574 \uB300\uD559\uC6D0\uC5D0 \uC9C4\uD559\uD588\uC2B5\uB2C8\uB2E4.\n      \uC6F9 \uAE30\uBC18 \uB370\uC774\uD130 \uBD84\uC11D \uC2DC\uC2A4\uD15C, \uBA38\uC2E0\uB7EC\uB2DD \uBAA8\uB378\uC758 \uC131\uB2A5 \uD3C9\uAC00, \uB124\uD2B8\uC6CC\uD06C \uC2DC\uAC01\uD654\uB97C \uC5F0\uAD6C\uD569\uB2C8\uB2E4."
+  }, {
+    "course": "학사",
+    "where": "아주대학교 미디어학과",
+    "href": "https://media.ajou.ac.kr/media/index.jsp",
+    "period": "2019-2019",
+    "story": "\uBBF8\uB514\uC5B4 \uCF58\uD150\uCE20 \uAE30\uD68D\uC790\uC774\uC790 \uAC1C\uBC1C\uC790\uB85C \uC131\uC7A5\uD558\uAE30 \uC704\uD55C \uC2DC\uAC04\uC774\uC5C8\uC2B5\uB2C8\uB2E4.\n      \uD504\uB85C\uC81D\uD2B8\uB97C \uC911\uC2EC\uC73C\uB85C \uC9C4\uD589\uB418\uC5C8\uB358 \uD559\uAD50 \uC0DD\uD65C \uB355\uBD84\uC5D0 \uB2E4\uC591\uD55C \uC9C0\uC2DD\uACFC \uAE30\uC220\uC744 \uC775\uD790 \uC218 \uC788\uC5C8\uC2B5\uB2C8\uB2E4.\n      \uC6F9 \uD504\uB860\uD2B8\uC5D4\uB4DC \uAC1C\uBC1C\uC744 \uC704\uD558\uC5EC \uD504\uB85C\uADF8\uB798\uBC0D\uC744 \uACF5\uBD80\uD588\uACE0, \uC778\uD130\uD398\uC774\uC2A4 \uB514\uC790\uC778\uC744 \uC218\uC6D4\uD558\uAC8C \uD558\uAE30 \uC704\uD574 \uB2E4\uC591\uD55C \uC5B4\uB3C4\uBE44 \uB514\uC790\uC778 \uD234\uC744 \uC775\uD614\uC2B5\uB2C8\uB2E4.\n      \uB354 \uC88B\uC740 \uCF58\uD150\uCE20\uB97C \uB9CC\uB4E4\uAE30 \uC704\uD558\uC5EC UX \uB514\uC790\uC778\uACFC \uB514\uC9C0\uD138 \uBBF8\uB514\uC5B4 \uC2EC\uB9AC\uD559\uC744 \uACF5\uBD80\uD588\uC2B5\uB2C8\uB2E4.\n      "
+  }]
+};
